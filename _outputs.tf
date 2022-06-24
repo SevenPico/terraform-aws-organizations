578347452e683919162b4ef83ac0878a0bfc37e1
Brad <bradleyrosenlof@gmail.com>
2022-06-24 14:56:23 -0500

    switch role url output

--- _outputs.tf
+++ _outputs.tf
@@ -1,5 +1,5 @@
-output "accounts" {
-  value = {
+locals {
+  accounts = {
     for k, v in var.accounts :
       k => {
         parent = {
@@ -14,6 +14,34 @@ output "accounts" {
         ]
       }
   }
+
+  accounts_flat = merge(
+    {
+      for k, v in local.accounts:
+        "${k}-${v.parent.name}" => v.parent.id
+    },
+    [
+      for k, v in local.accounts: {
+        for c in v.children:
+          "${k}-${c.name}" => c.id
+      }
+    ]...)
+}
+
+
+output "accounts" {
+  value = local.accounts
+}
+
+output "accounts_flat" {
+  value = local.accounts
+}
+
+output "swtichrole_urls" {
+  value = [
+    for name, id in local.accounts_flat:
+      "https://signin.aws.amazon.com/switchrole?account=${id}&roleName=${var.access_role_name}&displayName=${name}"
+  ]
 }
 
 output "access_role_name" {
