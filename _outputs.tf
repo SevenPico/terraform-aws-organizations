307359bfc4014756d78b84ea2b848c78d72d1b11
Dave Raring <d@7pi.co>
2022-07-27 13:10:17 -0500

    treat account id as string

--- _outputs.tf
+++ _outputs.tf
@@ -14,7 +14,7 @@ locals {
     for unit, level in var.account_hierarchy :
       unit => {
         parent = {
-          id      = aws_organizations_account.parent[unit].id
+          id      = "${aws_organizations_account.parent[unit].id}"
           name    = aws_organizations_account.parent[unit].name
           email   = aws_organizations_account.parent[unit].email
           alias   = var.account_hierarchy[unit].parent.alias != null ? var.account_hierarchy[unit].parent.alias : "${unit}-${replace(aws_organizations_account.parent[unit].name, ".", "-")}"
@@ -22,7 +22,7 @@ locals {
         }
         children = [
           for child in level.children: {
-            id      = aws_organizations_account.child[child.name].id
+            id      = "${aws_organizations_account.child[child.name].id}"
             name    = aws_organizations_account.child[child.name].name
             email   = aws_organizations_account.child[child.name].email
             alias   = local.temp_children_map[child.name].alias != null ? local.temp_children_map[child.name].alias : "${unit}-${replace(aws_organizations_account.child[child.name].name, ".", "-")}"
@@ -39,36 +39,36 @@ locals {
   account_names_flat = merge(
     {
       for unit, level in local.accounts:
-        "${level.parent.name}" => level.parent.id
+        "${level.parent.name}" => "${level.parent.id}"
     },
     [
       for unit, level in local.accounts: {
         for child in level.children:
-          "${child.name}" => child.id
+          "${child.name}" => "${child.id}"
       }
     ]...)
 
   account_profiles_flat = merge(
     {
       for unit, level in local.accounts:
-        "${level.parent.profile}" => level.parent.id
+        "${level.parent.profile}" => "${level.parent.id}"
     },
     [
       for unit, level in local.accounts: {
         for child in level.children:
-          "${child.profile}" => child.id
+          "${child.profile}" => "${child.id}"
       }
     ]...)
 
   account_aliases_flat = merge(
     {
      for unit, level in local.accounts:
-      "${level.parent.alias}" => level.parent.id
+      "${level.parent.alias}" => "${level.parent.id}"
     },
     [
      for unit, level in local.accounts: {
       for child in level.children:
-       "${child.alias}" => child.id
+       "${child.alias}" => "${child.id}"
     }
     ]...)
 }
